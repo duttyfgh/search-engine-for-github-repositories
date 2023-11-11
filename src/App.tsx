735b6906fed057079a613b96a9c2f9fcd6a7d120
dutyfgh <kastrula09@gmail.com>
2023-11-11 16:49:15 +0200

    change: routing

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
       <Header />
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/search-engine-for-github-repositories" element={<HomePage />} />
         <Route path="/favourites" element={<FavouritePage />} />
       </Routes>
 
